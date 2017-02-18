2f5fa31373a0c6b8fd8e981cf0f2bdda07df2135
Denis Baskovsky <denis@baskovsky.ru>
2017-02-18 10:25:49 +0300

    ASC sorting in download, 2 digital format date

--- src/database.entries.js
+++ src/database.entries.js
@@ -6,8 +6,8 @@ const $$ = require('./database.promise');
 function getAll(user_id) {
   return $$(
     `SELECT entry, date_added FROM entries 
-    WHERE 
-    user_id = $1`,
+    WHERE user_id = $1
+    ORDER BY date_added ASC`,
     [user_id]
   );
 }
@@ -17,12 +17,12 @@ function getAll(user_id) {
  * @param date {String} like 2016-12-01
  */
 function get(user_id, date) {
-  let from = `${date} 0:0:0`;
-  let until = `${date} 23:59:59`;
+  const from = `${date} 0:0:0`;
+  const until = `${date} 23:59:59`;
+
   return $$(
     `SELECT entry, date_added FROM entries
-    WHERE
-    (user_id = $1 AND date_added >= $2 AND date_added <= $3)`,
+    WHERE (user_id = $1 AND date_added >= $2 AND date_added <= $3)`,
     [user_id, from, until]
   );
 }
@@ -37,8 +37,7 @@ function get(user_id, date) {
 function post(user_id, entry, telegram_entry_id, date_modified, date_added = new Date()) {
   return $$(
     `INSERT INTO entries (user_id, entry, telegram_entry_id, date_modified, date_added) 
-    VALUES
-    ($1, $2, $3, $4, $5)`,
+    VALUES ($1, $2, $3, $4, $5)`,
     [user_id, entry, telegram_entry_id, date_modified, date_added]
   );
 }
@@ -53,8 +52,7 @@ function put(user_id, entry, date_modified, telegram_entry_id) {
   return $$(
     `UPDATE entries
     SET entry = $2, date_modified = $3
-    WHERE
-    (user_id = $1 AND telegram_entry_id = $4)`,
+    WHERE (user_id = $1 AND telegram_entry_id = $4)`,
     [user_id, entry, date_modified, telegram_entry_id]
   );
 }
@@ -65,8 +63,7 @@ function put(user_id, entry, date_modified, telegram_entry_id) {
 function clear(user_id) {
   return $$(
     `DELETE from entries
-    WHERE
-    user_id = $1`,
+    WHERE user_id = $1`,
     [user_id]
   );
 }
--- src/format.js
+++ src/format.js
@@ -1,11 +1,26 @@
 const crypt = require('./crypt');
-
+/**
+ *
+ * @param text {Number|String}
+ * @returns {string}
+ */
+function convertIn2DigitFormat(text) {
+  return ('0' + text).slice(-2);
+}
+/**
+ *
+ * @param entries {Array}
+ * @returns {string}
+ */
 function formatRows(entries) {
   let out = '';
   let currentDateStr = '';
   entries.forEach(data => {
     const dataDate = data.date_added;
-    const dateStr = `${dataDate.getDate()}.${dataDate.getMonth() + 1}.${dataDate.getFullYear()}`;
+    const DD = convertIn2DigitFormat(dataDate.getDate());
+    const MM = convertIn2DigitFormat(dataDate.getMonth() + 1);
+    const YYYY = dataDate.getFullYear();
+    const dateStr = `${DD}.${MM}.${YYYY}`;
     if (dateStr !== currentDateStr) {
       out += '\n' + dateStr + '\n';
     }
@@ -14,8 +29,10 @@ function formatRows(entries) {
     if (decodeEntry) {
       out += decodeEntry + '\n';
     }
+
     return out;
   });
+
   return out.trim();
 }
 
