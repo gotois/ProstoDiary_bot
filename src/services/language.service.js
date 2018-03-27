214bfffd315a953f09eea51fb2b833decfc7ce0a
Deni Baskovsky <baskovsky@ya.ru>
2018-03-27 12:52:05 +0300

    lint fix

--- src/services/language.service.js
+++ src/services/language.service.js
@@ -1,66 +1,63 @@
-// Imports the Google Cloud client library
-const language = require('@google-cloud/language');
-
+// const language = require('@google-cloud/language');
 
 // The text to analyze
-const text = 'dollars 100';
+// const text = 'dollars 100';
 
-const document = {
-  content: text,
-  type: 'PLAIN_TEXT',
-};
+// const document = {
+//   content: text,
+//   type: 'PLAIN_TEXT',
+// };
 
-(async () => {
-  // return
-  
-  // Instantiates a client
-  const client = new language.LanguageServiceClient();
-  
-  const results = await client.analyzeSyntax({document: document})
-  const syntax = results[0];
-  
-  console.log('Parts of speech:');
-  syntax.tokens.forEach(part => {
-    console.log(`${part.partOfSpeech.tag}: ${part.text.content}`);
-    console.log(`Morphology:`, part.partOfSpeech);
-  });
-  
-  
-  // client
-  // .analyzeEntitySentiment({document: document})
-  // .then(results => {
-  //   const entities = results[0].entities;
-  //
-  //   console.log(`Entities and sentiments:`);
-  //   entities.forEach(entity => {
-  //     console.log(`  Name: ${entity.name}`);
-  //     console.log(`  Type: ${entity.type}`);
-  //     console.log(`  Score: ${entity.sentiment.score}`);
-  //     console.log(`  Magnitude: ${entity.sentiment.magnitude}`);
-  //   });
-  // })
-  // .catch(err => {
-  //   console.error('ERROR:', err);
-  // });
-  
-  
-  // Detects the sentiment of the document
-  // client
-  // .analyzeSentiment({document: document})
-  // .then(results => {
-  //   const sentiment = results[0].documentSentiment;
-  //   console.log(`Document sentiment:`);
-  //   console.log(`  Score: ${sentiment.score}`);
-  //   console.log(`  Magnitude: ${sentiment.magnitude}`);
-  //
-  //   const sentences = results[0].sentences;
-  //   sentences.forEach(sentence => {
-  //     console.log(`Sentence: ${sentence.text.content}`);
-  //     console.log(`  Score: ${sentence.sentiment.score}`);
-  //     console.log(`  Magnitude: ${sentence.sentiment.magnitude}`);
-  //   });
-  // })
-  // .catch(err => {
-  //   console.error('ERROR:', err);
-  // });
-})()
+// (async () => {
+// return
+
+// Instantiates a client
+// const client = new language.LanguageServiceClient();
+
+// const results = await client.analyzeSyntax({document: document})
+// const syntax = results[0];
+
+// console.log('Parts of speech:');
+// syntax.tokens.forEach(part => {
+// console.log(`${part.partOfSpeech.tag}: ${part.text.content}`);
+// console.log(`Morphology:`, part.partOfSpeech);
+// });
+
+// client
+// .analyzeEntitySentiment({document: document})
+// .then(results => {
+//   const entities = results[0].entities;
+//
+//   console.log(`Entities and sentiments:`);
+//   entities.forEach(entity => {
+//     console.log(`  Name: ${entity.name}`);
+//     console.log(`  Type: ${entity.type}`);
+//     console.log(`  Score: ${entity.sentiment.score}`);
+//     console.log(`  Magnitude: ${entity.sentiment.magnitude}`);
+//   });
+// })
+// .catch(err => {
+//   console.error('ERROR:', err);
+// });
+
+
+// Detects the sentiment of the document
+// client
+// .analyzeSentiment({document: document})
+// .then(results => {
+//   const sentiment = results[0].documentSentiment;
+//   console.log(`Document sentiment:`);
+//   console.log(`  Score: ${sentiment.score}`);
+//   console.log(`  Magnitude: ${sentiment.magnitude}`);
+//
+//   const sentences = results[0].sentences;
+//   sentences.forEach(sentence => {
+//     console.log(`Sentence: ${sentence.text.content}`);
+//     console.log(`  Score: ${sentence.sentiment.score}`);
+//     console.log(`  Magnitude: ${sentence.sentiment.magnitude}`);
+//   });
+// })
+// .catch(err => {
+//   console.error('ERROR:', err);
+// });
+// })();
