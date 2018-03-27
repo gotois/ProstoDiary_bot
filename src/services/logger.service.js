d073ea6d83a9446fa02feee4ea99e5335c5c6eb0
Deni Baskovsky <baskovsky@ya.ru>
2018-03-27 11:30:14 +0300

    WIP: Coralogix Winston

--- src/services/logger.service.js
+++ src/services/logger.service.js
@@ -1,6 +1,7 @@
 const winston = require('winston');
+const CoralogixWinston = require('coralogix-logger-winston');
 const {PRODUCTION_MODE} = require('./../config/constants.config');
-const {NODE_ENV} = process.env;
+const {NODE_ENV, CORALOGIX_WINSTON_PRIVATE_KEY, CORALOGIX_WINSTON_APPLICATION_NAME} = process.env;
 
 const levels = {
   error: 0,
@@ -10,6 +11,15 @@ const levels = {
   debug: 4,
   silly: 5
 };
+
+// global configuration for coralogix
+const coralogixConfig = {
+  privateKey: CORALOGIX_WINSTON_PRIVATE_KEY,
+  applicationName: CORALOGIX_WINSTON_APPLICATION_NAME,
+  subsystemName: 'YOUR SUBSYSTEM',
+};
+
+CoralogixWinston.CoralogixTransport.configure(coralogixConfig);
 
 const logger = winston.createLogger({
   level: 'info',
@@ -26,6 +36,9 @@ const logger = winston.createLogger({
 if (NODE_ENV === PRODUCTION_MODE) {
   logger.add(new winston.transports.Console({
     format: winston.format.simple()
+  }));
+  logger.add(new CoralogixWinston.CoralogixTransport({
+    category: 'YOUR CATEGORY'
   }));
 }
 
