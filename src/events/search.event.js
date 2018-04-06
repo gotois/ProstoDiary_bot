4c97d0305ee838a71e8e6788f16c5b3b60098a14
Deni Baskovsky <baskovsky@ya.ru>
2018-04-06 10:44:55 +0300

    search pagination

--- src/events/search.event.js
+++ src/events/search.event.js
@@ -1,8 +1,19 @@
 const dbEntries = require('../database');
 const sessions = require('../services/session.service');
+const logger = require('../services/logger.service');
 const bot = require('../config');
 const {decodeRows} = require('../services/format.service');
 const {createRegexInput, normalizeRegexStringToString} = require('../services/input.service');
+/**
+ * @constant
+ * @type {number}
+ */
+const PAGE_SKIP = 10;// default every 10 times
+/**
+ * @constant
+ * @type {string}
+ */
+const NEXT_PAGE_VALUE = '__next_page';
 /**
  * @param date {Date}
  * @param entry {String}
@@ -22,6 +33,7 @@ const formatResponse = ({date, entry, matcher}) => {
  * @return {void}
  */
 const onSearch = async ({chat, from}, match) => {
+  logger.log('info', onSearch.name);
   const chatId = chat.id;
   const fromId = from.id;
   const currentUser = sessions.getSession(fromId);
@@ -33,12 +45,59 @@ const onSearch = async ({chat, from}, match) => {
     await bot.sendMessage(chatId, 'Not found');
     return;
   }
-  matchFilterRows.forEach(async ({entry, date}) => (
+  /**
+   * @param entry
+   * @param date
+   * @returns {Promise<void>}
+   */
+  const botSendMessage = async ({entry, date}) => {
     await bot.sendMessage(chatId, formatResponse({entry, date, matcher: input}), {
       'disable_web_page_preview': true,
       'parse_mode': 'Markdown',
-    })
-  ));
+    });
+  };
+  /**
+   * @param page {number}
+   * @returns {Promise<void>}
+   */
+  function* generateEntries(page) {
+    for (let i = 0; i < matchFilterRows.length; i += page) {
+      yield (matchFilterRows.slice(i, i + page));
+    }
+  }
+  /**
+   *
+   * @returns {Promise<void>}
+   */
+  const showNextEntries = async () => {
+    const nextEntries = generator.next();
+    if (!nextEntries.value) {
+      return;
+    }
+    const promises = nextEntries.value.map(async ({entry, date}) => {
+      await botSendMessage({entry, date});
+    });
+    await Promise.all(promises);
+    
+    if (nextEntries.value.length === PAGE_SKIP && !nextEntries.done) {
+      await bot.sendMessage(chatId, 'Show next entries', {
+        'reply_markup': {
+          'inline_keyboard': [
+            [
+              {'text': 'NEXT', 'callback_data': NEXT_PAGE_VALUE},
+            ]
+          ]
+        }
+      });
+      bot.once('callback_query', async (callbackQuery) => {
+        if (callbackQuery.data === NEXT_PAGE_VALUE) {
+          await showNextEntries();
+        }
+      });
+    }
+  };
+  const generator = generateEntries(PAGE_SKIP);
+  await showNextEntries();
 };
 
 module.exports = onSearch;
