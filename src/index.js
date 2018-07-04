0aa935b802fb6dd712438b66e021d8ca49a4693f
Deni Baskovsky <baskovsky@ya.ru>
2018-07-04 20:17:49 +0300

    checkAuth fn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 const client = require('./database/database.client');
 const logger = require('./services/logger.service');
 /**
+ * @returns {Promise<any>}
+ */
+const checkAuth = () => new Promise(async (resolve, reject) => {
+  // noinspection MagicNumberJS
+  const DELAY = 5000;
+  const timer = setTimeout(() => reject(new Error('Network unavailable')), DELAY);
+  const me = await require('./config').getMe();
+  clearTimeout(timer);
+  resolve(me);
+});
+/**
  * @param error {Object|undefined}
  */
 const connect = async error => {
@@ -8,7 +19,12 @@ const connect = async error => {
     logger.log({level: 'error', message: 'Connect error' + error});
     throw new Error('Connect error');
   }
-  await require('./config').getMe();
+  try {
+    await checkAuth();
+  } catch (error) {
+    logger.log('error', error.toString());
+    throw error;
+  }
   try {
     await require('./events');
     logger.log('info', 'server started');
