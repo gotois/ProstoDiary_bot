028b40e95bda809be40893ce948f4cc326e6b1bd
Deni Baskovsky <baskovsky@ya.ru>
2018-09-29 16:42:17 +0300

    add voice logger

--- src/events/voice.event.js
+++ src/events/voice.event.js
@@ -32,23 +32,22 @@ const getVoice = async ({chat, voice}) => {
   const chatId = chat.id;
   const fileInfo = await bot.getFile(voice.file_id);
   const buffer = await get(`https://api.telegram.org/file/bot${bot.token}/${fileInfo.file_path}`);
-  const audio = {
-    content: buffer
-  };
   const config = {
     encoding: convertTelegramMIMEToGoogleMIME(voice.mime_type), // raw 16-bit signed LE samples
     sampleRateHertz: 48000, // 48 khz
     languageCode: 'ru-RU', // a BCP-47 language tag
   };
   const request = {
-    audio: audio,
+    audio: {
+      content: buffer
+    },
     config: config,
   };
-  
+  logger.log('info', config);
   try {
     const [response] = await client.recognize(request);
     if (response && Array.isArray(response.results)) {
-      if (Array.isArray(response.results[0].alternatives)) {
+      if (Array.isArray(response.results[0] && response.results[0].alternatives)) {
         await bot.sendMessage(chatId, 'распознано: ' + response.results[0].alternatives[0].transcript);
         return;
       }
