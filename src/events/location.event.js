e09b98306df68d5301b2b0e5288352542d2a773e
Deni Baskovsky <baskovsky@ya.ru>
2019-02-16 18:14:00 +0300

    UnhandledPromiseRejectionWarning fix

--- src/events/location.event.js
+++ src/events/location.event.js
@@ -16,7 +16,26 @@ const onLocation = async ({chat, location: {latitude, longitude}}) => {
     await bot.sendMessage(chatId, 'Геокод не найден', {});
     return;
   }
-  const locShortName = parsedData.results[parsedData.results.length - 1].address_components[0].short_name;
+  /**
+   * @param {Object} parsedData - geocode parsed data
+   * @returns {*}
+   */
+  const getLocShortName = (parsedData) => {
+    const resultLength = parsedData.results.length;
+    if (!resultLength) {
+      logger.log('error', 'No results');
+      throw Error('No results');
+    }
+    try {
+      if (parsedData.results[resultLength - 1]) {
+        return parsedData.results[resultLength - 1].address_components[0].short_name;
+      }
+    } catch (error) {
+      logger.log('error', 'No address_components');
+      throw Error('No address_components');
+    }
+  };
+  const locShortName = getLocShortName(parsedData);
   const formattedAddress = parsedData.results[0].formatted_address;
   const parsedRestCountries = await getFullName(locShortName);
   if (!Array.isArray(parsedRestCountries)) {
