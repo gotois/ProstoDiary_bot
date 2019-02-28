923438a4456ea80ade402b16b101fedfc5e4fed2
Deni Baskovsky <baskovsky@ya.ru>
2019-02-28 11:08:47 +0300

    remove package scripts

--- SQL/table_users.sql
+++ SQL/table_users.sql
@@ -1,4 +1,7 @@
-﻿create table users (
+﻿-- TODO: переделать схему под приватную
+--create schema private;
+
+create table users (
   telegram_user_id integer primary key unique,
   date_added timestamp default current_timestamp
 );
