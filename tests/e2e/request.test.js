a7eac5cf6f85db68456521ed942d47f25920e8c4
Deni Baskovsky <baskovsky@ya.ru>
2019-05-02 12:35:21 +0300

    doctype lower case fix

--- tests/e2e/request.test.js
+++ tests/e2e/request.test.js
@@ -4,7 +4,7 @@ module.exports = async (t) => {
     get,
     // post, // TODO
   } = require('../../src/services/request.service');
-  const siteBuffer = await get('https://gotointeractive.com');
+  const siteBuffer = await get('https://prosto-diary.gotointeractive.com/');
   const html = siteBuffer.toString('utf8');
-  t.is(html.slice(0, 15), '<!doctype html>');
+  t.is(html.slice(0, 15).toLowerCase(), '<!doctype html>');
 };
