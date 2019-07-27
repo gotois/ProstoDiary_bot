f46410ffd8349a05dfac797327cf26f1f3def987
Deni Baskovsky <baskovsky@ya.ru>
2019-07-27 10:08:18 +0300

    refactor(sql): new scheme

--- SQL/install.sql
+++ SQL/install.sql
@@ -1,6 +1,8 @@
 --
 -- Create DB
 --
+-- FIXME: создать роль бота
+-- CREATE ROLE bot LOGIN;
 
 -- Warning! Set UTF-8 encoding: https://gist.github.com/ffmike/877447
 CREATE DATABASE "ProstoDiaryDB"
@@ -15,13 +17,12 @@ CREATE DATABASE "ProstoDiaryDB"
 COMMENT ON DATABASE "ProstoDiaryDB"
     IS 'Local DB';
 
-
 --
 -- Foods
 --
 
 -- Все на 100 гр. продукта
-CREATE TABLE IF NOT EXISTS Foods (
+CREATE TABLE IF NOT EXISTS foods (
   id SERIAL PRIMARY KEY,
   title TEXT UNIQUE, -- нужен индекс по этому
   protein NUMERIC (5, 2) default NULL,
@@ -38,31 +39,53 @@ SELECT to_tsvector('russian', $1) ||
 $$ LANGUAGE sql IMMUTABLE;
 
 -- Полнотекстовый поиск по тайтлу
-CREATE INDEX idx_gin_foods ON Foods
-  USING GIN (to_tsvector_multilang(title));
+CREATE INDEX idx_gin_foods ON Foods USING GIN (to_tsvector_multilang(title));
 
-
---
--- Users
---
-
--- TODO: переделать схему под приватную
---create schema private;
-CREATE TABLE IF NOT EXISTS Users (
-  telegram_user_id integer PRIMARY KEY UNIQUE,
-  date_added timestamp default current_timestamp
+-- История пользователя будет представлена как "процесс"
+CREATE TYPE intent AS ENUM (
+ 'buy',
+ 'eat',
+ 'finance',
+ 'fitness',
+ 'health',
+ 'meet',
+ 'pain',
+ 'todo',
+ 'weight',
+ 'work'
 );
 
-
+-- TODO: переделать схему под приватную и доступную для редактирования только роли бот
+--create schema private;
+CREATE TABLE IF NOT EXISTS bot_story (
+  id BIGSERIAL PRIMARY KEY,
+  version VARCHAR(20) NOT NULL, -- аналогична в package.json -нужна для проверки необходимости обновить историю бота
+--  sign -- подпись бота. todo: попробвать через `MD5('string');`?
+  contact JSONB NOT NULL,
+  publisher VARCHAR(100) NOT NULL, -- ссылка на репозиторий аналогична в package.json
+  jurisdiction JSONB,
+  telegram_user_id integer,
+  created_at timestamp default current_timestamp,
+  updated_at timestamp default NULL -- дата обновления (билда)
+);
 --
--- записи
+-- TODO: переделать схему под приватную и доступную для ...?
 --
-CREATE TABLE IF NOT EXISTS Entries (
-  id bigserial PRIMARY KEY,
-  user_id integer REFERENCES Users (telegram_user_id),
-  entry TEXT, -- каждый текст должен быть уникальным. если такой текст уже пользователь писал, то будет ссылка на него. таким образом можно достичь подсчета одинаковых действий
-  -- TODO: еще должен быть первоначальный, не обработанный текст
-  telegram_entry_id integer NOT NULL UNIQUE,
-  date_added timestamp default current_timestamp,
-  date_modified timestamp default NULL
+CREATE TABLE IF NOT EXISTS user_story (
+  id BIGSERIAL PRIMARY KEY,
+  type intent NOT NULL,
+  telegram_entry_id integer NOT NULL,
+  created_at timestamp default current_timestamp,
+  updated_at timestamp default NULL,
+  context JSONB
+);
+-- FIXME: нужен индекс для типа
+--CREATE INDEX idx_intent ON user_story USING GIN (type);
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE TABLE IF NOT EXISTS history (
+  id UUID PRIMARY KEY DEFAULT uuid_generate_v4() NOT NULL,
+  bot_story_id BIGSERIAL REFERENCES bot_story (id) NOT NULL,
+  user_story_id BIGSERIAL REFERENCES user_story (id) NOT NULL
+  --  status TEXT,
+  --  url
 );
