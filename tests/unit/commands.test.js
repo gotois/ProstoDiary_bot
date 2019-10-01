7e3158e92d69aae67294f585f6a51faf4f29dceb
Deni Baskovsky <baskovsky@ya.ru>
2019-10-01 10:34:15 +0300

    test(unit): commands

--- tests/unit/commands.test.js
+++ tests/unit/commands.test.js
@@ -21,13 +21,9 @@ module.exports = (t) => {
 
   t.true(BACKUP.alias instanceof RegExp);
   t.true(DBCLEAR.alias instanceof RegExp);
-  t.true(GETTODAY.alias instanceof RegExp);
   t.true(START.alias instanceof RegExp);
   t.true(HELP.alias instanceof RegExp);
-  t.true(GET.alias instanceof RegExp);
   t.true(SET.alias instanceof RegExp);
-  t.true(GRAPH.alias instanceof RegExp);
-  t.true(COUNT.alias instanceof RegExp);
   t.true(SEARCH.alias instanceof RegExp);
   t.true(VERSION.alias instanceof RegExp);
   t.true(typeof EDITED_MESSAGE_TEXT.alias === 'string');
@@ -37,26 +33,23 @@ module.exports = (t) => {
   t.true(SET.alias.test('/set 2018-03-18 something'));
   t.false(SET.alias.test('/set something'));
 
-  t.true(GETTODAY.alias.test('/get today'));
-  t.false(GETTODAY.alias.test('/get тудей'));
-
-  t.true(GET.alias.test('/get 2018-03-18'));
-  t.false(GET.alias.test('/get something'));
+  t.true(SEARCH.alias.test('/search something'));
+  // todo
+  // t.true(GETTODAY.search.test('/search что было сегодня?'));
+  // t.true(GET.search.test('/get показать 2018-03-18'));
+  // t.true(COUNT.alias.test('/count'));
+  // t.true(COUNT.alias.test('/count +'));
+  // t.true(COUNT.alias.test('/count -'));
+  // t.false(COUNT.alias.test('/count/'));
+  // t.false(COUNT.alias.test('/count-'));
+  // t.false(COUNT.alias.test('/count+'));
+  // todoend
 
   t.true(BACKUP.alias.test('/backup'));
   t.false(BACKUP.alias.test('/backup 1'));
 
   t.true(DBCLEAR.alias.test('/dbclear'));
   t.false(DBCLEAR.alias.test('/dbclear/'));
-
-  t.true(COUNT.alias.test('/count'));
-  t.true(COUNT.alias.test('/count +'));
-  t.true(COUNT.alias.test('/count -'));
-  t.false(COUNT.alias.test('/count/'));
-  t.false(COUNT.alias.test('/count-'));
-  t.false(COUNT.alias.test('/count+'));
-
-  t.true(SEARCH.alias.test('/search something'));
 
   t.true(LOCATION.alias === 'location');
 
