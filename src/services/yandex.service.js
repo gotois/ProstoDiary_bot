349391477eefa141a9d3b635fc27e1e855c16124
Deni Baskovsky <baskovsky@ya.ru>
2019-11-22 21:48:57 +0300

    fix(facebook oauth): graph me

--- src/services/yandex.service.js
+++ src/services/yandex.service.js
@@ -1,15 +1,45 @@
 const { get } = require('../services/request.service');
-const HOST = 'login.yandex.ru';
-
-const passportInfo = async (accessToken) => {
-  const response = await get(`https://${HOST}/info`, {
-    format: 'json',
-  }, {
-    Authorization: `OAuth ${accessToken}`
-  }, 'utf8');
+/**
+ * @param {object} response - grant response
+ * @param {string} response.access_token - yandex access token
+ * @param {object} response.raw - yandex raw response
+ * @returns {Promise<string|Buffer|Error|*>}
+ */
+const yandexPassportInfo = async ({ access_token }) => {
+  const HOST = 'login.yandex.ru';
+  const response = await get(
+    `https://${HOST}/info`,
+    {
+      format: 'json',
+    },
+    {
+      Authorization: `OAuth ${access_token}`,
+    },
+    'utf8',
+  );
   return response;
+};
+/**
+ * @param {object} response - grant response
+ * @param {string} response.name - name
+ * @param {object} response.id - facebook id
+ * @returns {Promise<string|Buffer|Error|*>}
+ */
+const facebookPassportInfo = async ({ access_token }) => {
+  const HOST = 'graph.facebook.com';
+  const response = await get(
+    `https://${HOST}/v5.0/me`,
+    {
+      access_token,
+      fields: 'email,id,gender,location,work,languages,name',
+    },
+    null,
+    'utf8',
+  );
+  return JSON.parse(response);
 };
 
 module.exports = {
-  passportInfo,
+  yandexPassportInfo,
+  facebookPassportInfo,
 };
