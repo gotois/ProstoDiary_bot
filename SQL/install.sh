6360bce1aba940f1df090d9d1071c1d6604a3219
Deni Baskovsky <baskovsky@ya.ru>
2019-11-30 08:03:25 +0300

    fix: ci postgresql

--- SQL/install.sh
+++ SQL/install.sh
@@ -6,10 +6,10 @@ psql --version;
 psql -U postgres -d postgres -a -f database.sql;
 
 # passport
-psql -U postgres -d postgres -a -f passport/tables/passport.sql;
-psql -U postgres -d postgres -a -f passport/tables/ld.sql;
-psql -U postgres -d postgres -a -f passport/tables/bot.sql;
+psql -U postgres -d postgres -a -f ./passport/tables/passport.sql;
+psql -U postgres -d postgres -a -f ./passport/tables/ld.sql;
+psql -U postgres -d postgres -a -f ./passport/tables/bot.sql;
 
 # message
-psql -U postgres -d postgres -a -f messages/tables/message.sql;
-psql -U postgres -d postgres -a -f messages/tables/abstract.sql;
+psql -U postgres -d postgres -a -f ./messages/tables/message.sql;
+psql -U postgres -d postgres -a -f ./messages/tables/abstract.sql;
--- SQL/messages/tables/message.sql
+++ SQL/messages/tables/message.sql
@@ -1,9 +1,13 @@
 CREATE TABLE IF NOT EXISTS message (
 id BIGSERIAL,
-uid TEXT, -- todo UID письма может повторяться, предусмотреть это
+uid TEXT, -- todo UID письма может повторяться, предусмотреть это или вести поле provider_email
 url TEXT UNIQUE, -- ссылка на email attachment
 telegram_message_id BIGINT CONSTRAINT must_be_different UNIQUE, -- сообщение телеграмма
+
+-- sign SOMEHASH -- todo электронная подпись сгенерированная ботом, которая подтверждает что бот не был скомпроментирован. todo: попробвать через `MD5('string');`?
+
 UNIQUE(telegram_message_id),
 PRIMARY KEY (id)
-);/Users/macbook/WebstormProjects/ProstoDiary_bot/src/api/v3/oauth.js
--- todo нужна VIEW которая будет отдавать bytea сырых данных - raw таблицы
+);
+
+GRANT ALL PRIVILEGES ON message TO bot;
