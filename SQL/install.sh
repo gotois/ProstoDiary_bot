3f707dfd5c769008c7ff3ffb1ad11a7d0d8c83a8
Deni Baskovsky <baskovsky@ya.ru>
2019-11-30 08:25:17 +0300

    fix(ci): prepare

--- SQL/install.sh
+++ SQL/install.sh
@@ -2,14 +2,17 @@
 
 psql --version;
 
+createuser -s $PGUSER
+psql -c 'create database $PGDATABASE ;' -U postgres
+
 # create database with extensions
-psql -U postgres -d postgres -a -f database.sql;
+psql -U $PGUSER -d $PGDATABASE -a -f database.sql;
 
 # passport
-psql -U postgres -d postgres -a -f ./passport/tables/passport.sql;
-psql -U postgres -d postgres -a -f ./passport/tables/ld.sql;
-psql -U postgres -d postgres -a -f ./passport/tables/bot.sql;
+psql -U $PGUSER -d $PGDATABASE -a -f ./passport/tables/passport.sql;
+psql -U $PGUSER -d $PGDATABASE -a -f ./passport/tables/ld.sql;
+psql -U $PGUSER -d $PGDATABASE -a -f ./passport/tables/bot.sql;
 
 # message
-psql -U postgres -d postgres -a -f ./messages/tables/message.sql;
-psql -U postgres -d postgres -a -f ./messages/tables/abstract.sql;
+psql -U $PGUSER -d $PGDATABASE -a -f ./messages/tables/message.sql;
+psql -U $PGUSER -d $PGDATABASE -a -f ./messages/tables/abstract.sql;
