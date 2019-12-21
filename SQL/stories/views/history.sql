9ec4391790ae1069e6a89cdbaf8d9df03e37802b
Deni Baskovsky <baskovsky@ya.ru>
2019-12-22 00:45:03 +0300

    fix(sql): aggregate categories

--- SQL/stories/views/history.sql
+++ SQL/stories/views/history.sql
@@ -8,8 +8,12 @@ SELECT
   passport.user.email AS creator_email,
   passport.bot.email AS publisher_email,
   story.content.content,
+  array_agg(story.abstract.category) AS categories,
+  story.content.status,
   story.content.content_type
 FROM story.message
 INNER JOIN passport.user ON passport.user.id = story.message.creator
 INNER JOIN passport.bot ON passport.bot.id = story.message.publisher
-INNER JOIN story.content ON story.content.message_id = story.message.id;
+INNER JOIN story.content ON story.content.message_id = story.message.id
+INNER JOIN story.abstract ON story.abstract.content_id = story.content.id
+GROUP BY story.message.id, story.content.id, passport.user.id, passport.bot.id;
