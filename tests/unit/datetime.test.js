bb4a91e26f1b8fc9fd581a212eafac089620d037
Deni Baskovsky <baskovsky@ya.ru>
2020-03-22 13:48:53 +0300

    fix(unit test): ava 3 throws
    fix #503

--- tests/unit/datetime.test.js
+++ tests/unit/datetime.test.js
@@ -21,7 +21,7 @@ module.exports = (t) => {
   t.is(convertToNormalDate('2017-08-07').getMonth(), 8);
   t.throws(() => {
     convertToNormalDate('2017-27-2');
-  }, Error);
+  }, { instanceOf: Error });
   t.is(convertToNormalDate(new Date(0)).getMonth(), 0);
   t.is(convertToNormalDate(new Date(0)).toDateString(), 'Thu Jan 01 1970');
   t.true(convertToNormalDate(new Date()) instanceof Date);
