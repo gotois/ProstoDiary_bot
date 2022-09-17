3c6c69fa3c38f1066ebbdc7745aa814cef146f9d
Denis Baskovsky <denis@baskovsky.ru>
2022-09-17 18:07:08 +0300

    Перенес email-assistant

--- src/include/email-bot/services/smtp-transport.js
+++
@@ -1,6 +0,0 @@
-const nodemailer = require('nodemailer');
-const environment = require('../environment.json');
-
-const transporter = nodemailer.createTransport(environment.smtp);
-
-module.exports = transporter;
