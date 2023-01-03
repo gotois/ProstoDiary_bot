0638d1449f4fb40dc6cc5fe9fa7ca19e10653ced
Denis Baskovsky <denis@baskovsky.ru>
2023-01-03 14:45:51 +0300

    remove odd code

--- src/include/telegram-bot/controllers/group-chat-created.event.js
+++
@@ -1,9 +0,0 @@
-const TelegramMessage = require('../models/telegram-bot-message');
-const logger = require('../../../lib/log');
-/**
- * @description Создание чата
- * @param {TelegramMessage} message - msg
- */
-module.exports = (message) => {
-  logger.info(message);
-};
