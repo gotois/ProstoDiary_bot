51877b75e513d2d6538180788439d3b45962300a
Denis Baskovsky <denis@baskovsky.ru>
2023-01-04 23:35:02 +0300

    remove odd code

--- src/app/routes/api.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const jsonParser = require('body-parser').json({ limit: '1mb' });
-const tokenAuth = require('../middlewares/token-auth');
-const rpcAPI = require('../controllers/web/jsonrpc');
-
-const router = express.Router();
-router.post('*', jsonParser, tokenAuth, rpcAPI);
-
-module.exports = router;
--- src/lib/redis.js
+++
@@ -1,9 +0,0 @@
-const Redis = require('ioredis');
-
-const redis = new Redis({
-  port: 6379, // Redis port
-  host: '127.0.0.1', // Redis host
-  db: 0, // Defaults to 0
-});
-
-module.exports = redis;
