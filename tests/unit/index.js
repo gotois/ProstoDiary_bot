a14549918f5f41f01675771bce4b070590dd4e6a
Denis Baskovsky <denis@baskovsky.ru>
2023-01-06 23:46:33 +0300

    package upgrade and snyk off

--- tests/unit/index.js
+++ tests/unit/index.js
@@ -1,17 +1,3 @@
-const {
-  test,
-  skipTestForFast,
-} = require('../helpers');
+const test = require('ava');
 
-test('database config', require('./dbconfig.test'));
-test('helpers', require('./helpers.test'));
-test('jsonld', require('./jsonld.test'));
-test('logger', require('./log.test'));
-test('crypto', require('./crypto.test'));
-test('commands', require('./commands.test'));
-test('datetime', require('./date.test'));
-test('format', require('./format.test'));
-test('env', require('./environment.test'));
-test('faker', require('./faker.test'));
-test('request', require('./request.test'));
-skipTestForFast('config', require('./config.test'));
+test('config', require('./config.test'));
