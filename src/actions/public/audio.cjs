31507d13ca1e71eb12125d3fbaeeffec0dc20b5d
Denis Baskovsky <denis@baskovsky.ru>
2024-12-10 11:28:56 +0300

    WIP любой ввод должен попадать в метод chat

--- src/actions/public/audio.cjs
+++ src/actions/public/audio.cjs
@@ -1,10 +1,16 @@
 const Dialog = require('../../libs/dialog.cjs');
 const {
-  generateCalendar,
+  sentToSecretary,
   formatCalendarMessage,
   formatGoogleCalendarUrl,
 } = require('../../controllers/generate-calendar.cjs');
-const { sendCalendarMessage, sendTaskMessage, sendErrorMessage } = require('../../libs/tg-messages.cjs');
+const {
+  sendCalendarMessage,
+  sendPrepareMessage,
+  sendTaskMessage,
+  sendErrorMessage,
+} = require('../../libs/tg-messages.cjs');
+const { serializeMarkdownV2 } = require('../../libs/md-serialize.cjs');
 const { saveCalendar } = require('../../libs/database.cjs');
 const { notify } = require('../../libs/execute-time.cjs');
 
@@ -12,17 +18,42 @@ module.exports = async (bot, message, user) => {
   try {
     const dialog = new Dialog();
     dialog.push(message);
-    const ical = await generateCalendar({
+    const { data, type } = await sentToSecretary({
       id: dialog.uid,
       activity: dialog.activity,
       jwt: user.jwt,
+      language: dialog.language,
     });
-    const output = formatCalendarMessage(ical, dialog.language);
-    const googleCalendarUrl = formatGoogleCalendarUrl(ical);
-    const calendarMessage = await sendCalendarMessage(bot, message, output, googleCalendarUrl);
-    await saveCalendar(calendarMessage.message_id, user.key, ical);
-    const { text, url } = await notify(ical);
-    await sendTaskMessage(bot, calendarMessage, text, url);
+    await sendPrepareMessage(bot, message);
+    switch (type) {
+      case 'text/markdown': {
+        await bot.sendMessage(message.chat.id, serializeMarkdownV2(data), {
+          parse_mode: 'MarkdownV2',
+          reply_to_message_id: message.message_id,
+          protect_content: true,
+        });
+        break;
+      }
+      case 'text/plain': {
+        await bot.sendMessage(message.chat.id, data, {
+          reply_to_message_id: message.message_id,
+          protect_content: true,
+        });
+        break;
+      }
+      case 'text/calendar': {
+        const output = formatCalendarMessage(data, dialog.language);
+        const googleCalendarUrl = formatGoogleCalendarUrl(data);
+        const calendarMessage = await sendCalendarMessage(bot, message, output, googleCalendarUrl.href);
+        await saveCalendar(calendarMessage.message_id, user.key, data);
+        const { text, url } = await notify(data);
+        await sendTaskMessage(bot, calendarMessage, text, url);
+        break;
+      }
+      default: {
+        throw new Error('Unknown type ' + type);
+      }
+    }
   } catch (error) {
     console.error(error);
     await sendErrorMessage(bot, message, error);
