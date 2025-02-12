a995303115da48404ca6f9e4e5402b55b25da648
Denis Baskovsky <denis@baskovsky.ru>
2025-02-12 18:42:59 +0300

    publix text fix

--- src/actions/public/text.cjs
+++ src/actions/public/text.cjs
@@ -1,33 +1,43 @@
-const Dialog = require('../../libs/dialog.cjs');
-const { TEXT_CALENDAR } = require('../../libs/mime-types.cjs');
-
-module.exports = async (bot, message) => {
-  const dialog = new Dialog();
-  await dialog.push(message);
-  const accept = 'text/calendar';
-
-  switch (queryResult.intent.displayName) {
-    case 'OrganizeAction': {
-      await bot.sendMessage(message.chat.id, queryResult.fulfillmentText, {
-        reply_to_message_id: message.message_id,
-        disable_notification: false,
-        disable_web_page_preview: true,
-        reply_markup: {
-          force_reply: true,
-          inline_keyboard: [
-            [
-              {
-                text: 'Подтвердить',
-                callback_data: 'approve_event',
-              },
-            ],
-          ],
-        },
-      });
-      break;
-    }
-    default: {
-      break;
-    }
-  }
+module.exports = async (/*bot, message, dialog*/) => {
+  // dialog.push(message);
+  // await bot.sendMessage(message.chat.id, 'output', {
+  //   reply_markup: {
+  //    inline_keyboard: [
+  //      [
+  //        {
+  //          text: 'Начать',
+  //          'url': 'https://google.com',
+  //        },
+  //      ],
+  //    ],
+  //  },
+  //  });
+  //
+  // const x = await bot.getUpdates({
+  //   allowed_updates: ['message_reaction'],
+  // });
+  // console.log(x)
+  // switch (queryResult.intent.displayName) {
+  //   case 'OrganizeAction': {
+  //     await bot.sendMessage(message.chat.id, queryResult.fulfillmentText, {
+  //       reply_to_message_id: message.message_id,
+  //       disable_notification: false,
+  //       disable_web_page_preview: true,
+  //       reply_markup: {
+  //         inline_keyboard: [
+  //           [
+  //             {
+  //               text: 'Подтвердить',
+  //               callback_data: 'approve_event',
+  //             },
+  //           ],
+  //         ],
+  //       },
+  //     });
+  //     break;
+  //   }
+  //   default: {
+  //     break;
+  //   }
+  // }
 };
