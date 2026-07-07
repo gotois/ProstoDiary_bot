497307da31e021a77dbe5ae36276a8c97d8e3420
qertis <qertis@users.noreply.github.com>
2026-07-07 10:37:24 +0300

    refactor(test)!: add tests for YAML configuration validation

--- tests/unit/config.test.ts
+++ tests/unit/config.test.ts
@@ -1,17 +1,11 @@
 import fs from 'node:fs';
 import validator from 'validator';
-import yaml from 'js-yaml';
 
 /**
  * @param {import('ava').ExecutionContext} t - ava test
  */
 export default (t) => {
   t.log('testing');
-  const configYML = fs.readFileSync(new URL('../../../compose.yml', import.meta.url), 'utf8');
-  t.notThrows(() => {
-    const config = yaml.load(configYML);
-    t.true(validator.isJSON(JSON.stringify(config)));
-  });
   const prettierrcJSON = fs.readFileSync(new URL('../../.prettierrc', import.meta.url), 'utf8');
   t.true(validator.isJSON(prettierrcJSON));
   t.pass();
